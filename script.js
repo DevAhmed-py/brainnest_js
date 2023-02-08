21004f0bf33ace3b9d3293ea6e340881a6b29de2
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2023-02-08 21:20:48 +0100

    improved

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 let computerScore = 0
 let playerScore = 0
 
+function game() {
+    for (let i = 0; i < 5; i ++) {
+        let player = playerChoice();
+        let computer = computerPlay()
+        console.log(playRound(player, computer))
+    }
+
+    if (computerScore > playerScore) {
+        return `You LOST! Computer score: ${computerScore} VS Your Score: ${playerScore}`
+    } else if (computerScore < playerScore) {
+        return `You WON! Computer score: ${computerScore} VS Your Score: ${playerScore}`
+    } else {
+        return `It's a DRAW! Computer score: ${computerScore} VS Your Score: ${playerScore}`
+    }
+}
+
+function playerChoice() {
+    const playerOptions = ["rock", 'paper', 'scissors']
+    let player = prompt('Rock, Paper or Scissor').toLowerCase();
+    if (playerOptions.includes(player)) {
+        return player
+    } else {
+        console.log("Please enter a valid input") 
+    }
+}
+
 function computerPlay() {
     let randNum = Math.floor(Math.random() * 3) + 1
     switch (randNum) {
@@ -48,21 +74,19 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-
-    for (let i = 0; i < 5; i ++) {
-        let player = prompt('Rock, Paper or Scissor').toLowerCase();
-        let computer = computerPlay()
-        console.log(playRound(player, computer))
-    }
-
-    if (computerScore > playerScore) {
-        return `You LOST! Computer score: ${computerScore} VS Your Score: ${playerScore}`
-    } else if (computerScore < playerScore) {
-        return `You WON! Computer score: ${computerScore} VS Your Score: ${playerScore}`
-    } else {
-        return `It's a DRAW! Computer score: ${computerScore} VS Your Score: ${playerScore}`
-    }
-}
-
 console.log(game())
+
+// function playRound(playerSelection, computerSelection) {
+//     if (playerSelection == computerSelection) {
+//         return "It's a DRAW! Play Again!!"
+
+//     } else if (
+//         (playerSelection == 'rock' && computerSelection == 'scissors') ||
+//         (playerSelection == 'paper' && computerSelection == 'rock') || 
+//         (playerSelection == 'scissors' && computerSelection == 'paper')) {
+//         return 'Player'
+        
+//     } else {
+//         return 'Computer'
+//     }
+// }
